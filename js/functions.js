34bb7396c1468ecf0445b244f362a7b805ee94b6
Majuniemi <149418950+Majuniemi@users.noreply.github.com>
2024-02-23 05:52:15 +0200

    Header ja footer lisätty

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([0, 0], 1)
+var map = L.map('map').setView([65.5, 27.055430], 6)
 
 L.tileLayer('https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=GdSPu31pwUy9cLlOnBAA', {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
